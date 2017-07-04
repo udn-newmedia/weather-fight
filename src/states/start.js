0850459f24fda489a1ad99b31e3e2b56c5d95e0d
Taylor Fang <stu005606@gmail.com>
2017-07-04 15:56:00 +0800

    Add start button and longform button on start page

--- src/states/start.js
+++ src/states/start.js
@@ -2,28 +2,48 @@
 
 let StartState = {
 
-    init: function(){
-
-        this.game.stage.backgroundColor = '#000'
-
-        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL
-
-        this.scale.pageAlignHorizontally = true
-        this.scale.pageAlignVertically = true
-
-    },
-
-    preload: function(){
-
-        this.game.load.image('preload','../public/assets/preloader.gif')
-
-    },
-
     create: function(){
-        // this.game.stage.disableVisibilityChange = true; //continuing when the page is unfocused
         
-        this.game.state.start('Load')
+        var bg = this.game.add.image(0,0,'bg')
+        bg.width = this.game.world.width
+        bg.height = this.game.world.height
 
+        var title = this.game.add.image(0,0,'title')
+        title.width = this.game.world.width * 1.1
+        title.height = this.game.world.height * 0.6
+        
+        var startbtn = this.game.add.button(this.game.world.width * 0.5, this.game.world.height * 0.8, 'startgame_btn', this.onStartClick, this, 1,1,0)
+        startbtn.anchor.setTo(0.5,0.2)
+        startbtn.width = 150
+        startbtn.height = 48
+
+        var longformbtn = this.game.add.button(this.game.world.width * 0.5, this.game.world.height * 0.9, 'longform_btn', this.onLongformClick, this, 1,1,0)
+        longformbtn.anchor.setTo(0.5,0.4)
+        longformbtn.width = 150
+        longformbtn.height = 48
+
+    },
+
+    update: function(){
+    },
+
+    onStartClick: function() {
+        // this.game.state.start('Play')
+        var startbtn_click = this.game.add.button(this.game.world.width * 0.5, this.game.world.height * 0.8, 'startgame_btn_2', this.onStartClick, this, 1,1,0)
+        startbtn_click.anchor.setTo(0.5,0.2)
+        startbtn_click.width = 150
+        startbtn_click.height = 48
+
+        console.log("start")
+    },
+
+    onLongformClick: function() {
+        var startbtn_click = this.game.add.button(this.game.world.width * 0.5, this.game.world.height * 0.9, 'longform_btn_1', this.onStartClick, this, 1,1,0)
+        startbtn_click.anchor.setTo(0.5,0.4)
+        startbtn_click.width = 150
+        startbtn_click.height = 48
+
+        console.log("longform")
     }
 
 }
