1eb63c94ff8a97f8c3f6fc8968c42c1786698dab
Taylor Fang <stu005606@gmail.com>
2017-07-27 17:06:26 +0800

    fix fb button disappear after clicking share

--- src/states/over.js
+++ src/states/over.js
@@ -403,7 +403,9 @@ let OverState = {
         var anchor_y = 0.2
         var txt_anchor_y = 0
 
-        var style = isClick?btnStyle+'_click':btnStyle
+        if(value!=='分享出去吧!'){
+            var style = isClick?btnStyle+'_click':btnStyle
+        }
 
         var button = this.game.add.button(x, y, style, function() {
             this.btnGenerator(style, value, position, true)
